02384ba7517f06505fec6fa9bcd4d6c7632660ac
Rafi <rafirasheed.tc99@gmail.com>
2022-06-21 08:37:27 +0530

    implimented verification methods

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 
 #from crypt import methods
+from crypt import methods
 from operator import methodcaller
 from flask import Flask, jsonify, render_template, request
 import os
+from matplotlib.pyplot import prism
 from werkzeug.utils import secure_filename
 import json
 import pandas as pd
@@ -10,16 +12,19 @@ import numpy as np
 from keras.models import model_from_json
 import datetime
 from flask_cors import CORS
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 app = Flask(__name__)
 CORS(app)
-json_file = open('CNNmodel.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("CNNmodel.h5") 
-print("Loaded model from disk")  
-loaded_model.summary()
+# json_file = open('CNNmodel.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# loaded_model = model_from_json(loaded_model_json)
+# loaded_model.load_weights("CNNmodel.h5") 
+# print("Loaded model from disk")  
+# loaded_model.summary()
 
 database_url = "https://rain-pred-default-rtdb.firebaseio.com"
 
@@ -31,9 +36,11 @@ firebase_admin.initialize_app(cred, {
 	'databaseURL':database_url
 })
 
+kerala_dist = ["Nothing","Kasaragod","Kannur","Wayanad","Kozhikode","Malappuram","Palakkad","Thrissur","Ernakulam","Idukki","Kottayam","Alappuzha","Pathanamthitta","Kollam","Thiruvananthapuram"]
+
 @app.route('/')
 def hello_world():
-	return 'Hello World'
+	return render_template('index.html')
 
 
 @app.route('/test_api',methods=['GET','POST'])            
@@ -114,29 +121,132 @@ def getValue():
 	print(k)
 	return jsonify(k)
 
+@app.route("/formotp", methods=["GET"])
+def form():
+	return render_template('otp.html')
+
+from itsdangerous import URLSafeTimedSerializer
+import time
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer("12ewewjindcdsk")
+    return serializer.dumps(email, salt="qwertyui")
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer("12ewewjindcdsk")
+    try:
+        email = serializer.loads(
+            token,
+            salt="qwertyui",
+            max_age=expiration
+        )
+    except:
+        return "Time Expired "
+    return email
+
+@app.route("/otp", methods=["GET"])
+def otp():
+	a = generate_confirmation_token("9747165032")
+	print(a)
+	time.sleep(2)
+	return(confirm_token(a, 3600))
+
 @app.route('/value', methods=["POST"])
 def storeValue():
 	name = request.form.get("name")
 	pin = request.form.get("pin")
 	mobile = request.form.get("mobile")
-	if name == "" or pin == "" or mobile =="":
-		print("no entry found ")
-		return "error while registration" 
+	if name == "" or name == " ":
+		return "cant find name field"
+	if pin == "" or pin == " ":
+		return "cant find Pin code"
+	if mobile == "" or mobile == " ":
+		return "cant find mobile number"
+	#pin code test
+	# value = requests.get("")
+	session = requests.Session()
+	retry = Retry(connect=3, backoff_factor=0.5)
+	adapter = HTTPAdapter(max_retries=retry)
+	session.mount('http://', adapter)
+	session.mount('https://', adapter)
+	URL = "https://api.postalpincode.in/pincode/"+ pin
+	value = session.get(URL)
+	#print(value.content)
+	my_json = value.content.decode('UTF-8')
+	my_json = my_json[1:-1]
+	my_json = json.loads(my_json)
+	try:
+		state = my_json.get("PostOffice")[0]
+	except Exception as e:
+		return("invalid pincode")
+	state = state.get("State")
+	print(state)
+	a = 0
+	if state == "Kerala":
+		dist = (my_json.get("PostOffice")[0]).get("District")
+		a = kerala_dist.index(dist)
+		print(a)
+	else:
+		return "Not in kerala"
+
 	ref = db.reference("/")
 	ref.push({
     "user": {
             "name": name,
             "pin": pin,
+			"dist": a,
             "mobile": mobile,
+			"otp": False,
   }
 })
-	return "Done"
+	otp_token = generate_confirmation_token(mobile)
+	sms = "click here to verify your mobile number with in 1Hr. http://localhost:600/token?key="+otp_token
+	print(sms)
+	#send sms 
+	return "An OTP send to your Mobile Number. Click that to verify your Mobile Number | " + sms 
+
+def updateEntryinOTP(mobileNumber):
+	ref = db.reference("/")
+	j_value = ref.get()
+	for key in j_value:
+		
+		mobile = j_value.get(key).get('user').get('mobile')
+		print(mobile)
+		if mobile == mobileNumber:
+			print("found match")
+			name = j_value.get(key).get('user').get('name')
+			pin = j_value.get(key).get('user').get('pin')
+			a = j_value.get(key).get('user').get('dist')
+			ref.child(key).update({
+				"user": {
+            		"name": name,
+            		"pin": pin,
+					"dist": a,
+         			"mobile": mobile,
+					"otp": True,
+  }})
+			return "You are succesfully authenticated your Mobile number"
+	else:
+		return "Something went wrong"
+	
+@app.route('/token/', methods=["GET"])
+def verify():
+	key = request.args.get('key')
+	print(key)
+	try:
+		mob = confirm_token(key)
+	except:
+		return('The confirmation link is invalid or has expired.', 'danger')
+	print("mob : ", mob)
+	return(updateEntryinOTP(mob))
 
 @app.route('/file', methods=["POST"])
 def getfile():
 	f = request.files['file'] 
 	f.save(f.filename)  
 	return "done"
+
+
 
 if __name__ == '__main__':
 	app.run(port=600, host="0.0.0.0", debug=True)
